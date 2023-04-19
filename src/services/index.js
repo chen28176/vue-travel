c18039a52299dae34e98a69d4202c54f12a778f5
86 <10976595+chenruiho@user.noreply.gitee.com>
2023-04-19 19:55:06 +0800

    request引入order

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,12 @@
 /*
  * @Author: '陈28176' 'chen28176@qq.com'
  * @Date: 2023-02-23 22:10:10
- * @LastEditors: '陈28176' 'chen28176@qq.com'
- * @LastEditTime: 2023-03-06 16:42:36
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2023-04-19 19:54:33
  * @Description: 
  */
 export * from "./modules/city"
 export * from "./modules/home"
 export * from "./modules/detail"
 export * from "./modules/favor"
+export * from "./modules/order"
