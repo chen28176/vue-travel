4822d553739d0ee723a117e5f5509258a549c8f6
86 <10976595+chenruiho@user.noreply.gitee.com>
2023-04-19 19:44:01 +0800

    收藏页重构

---
+++ src/views/favor/cpns/favor-list-item.vue
@@ -0,0 +1,148 @@
+<template>
+      <div class="favor-list-item">
+        <!-- 上方图片 -->
+        <div class="top-img">
+          <img :src="itemData.defaultPicture">
+          <div class="info">
+            <span>{{itemData.commentBrief.overall}}分 / {{itemData.commentBrief.scoreTitle}}</span>
+          </div>
+        </div>
+        <!-- 下方信息 -->
+        <div class="bottom-info">
+          <div class="title">
+            <div class="info">
+              <span>{{itemData.unitInfor}}</span>
+              <span>{{itemData.unitSummeries[0]?.text }}-{{itemData.unitSummeries[1]?.text }}</span>
+            </div>
+            <div class="title-h2">
+              {{itemData.unitName}}
+            </div>
+          </div>
+          <div class="price-info">
+            <div class="info-hint">
+              <span  v-for="(iten,indey) in itemData.houseTags" :key="indey">{{iten.text}}</span>
+            </div>
+            <div class="info-price">
+              <span>￥{{ itemData.finalPrice }}</span>
+              <span>￥{{itemData.productPrice}}</span>
+              <div class="notice" v-if="itemData.priceTipBadge?.text">
+                  {{ itemData.priceTipBadge?.text }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script setup>
+defineProps({
+  itemData: {
+        type: Object,
+    }
+})
+</script>
+
+<style lang="less" scoped>
+.favor-list-item {
+  border-radius: 10px;
+  padding: 10px;
+  border-bottom: 1px solid #d6d6d8;
+  margin: 10px;
+  .top-img {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+      width: 363px;
+      height: 20vh;
+      border-radius: 2vh;
+    }
+    .info {
+      width: 80px;
+      margin-left: 10px;
+      font-size: 10px;
+      text-align: center;
+      padding: 5px;
+      position: relative;
+      bottom: 170px;
+      right: 130px;
+      border-radius: 10px;
+      color: #000;
+      font-weight: bold;
+      background: #e8e8e8;
+    }
+  }
+
+  .bottom-info {
+    .title {
+      margin-top: 10px 10px;
+
+      .info {
+        margin: 5px 10px;
+        span:nth-child(1) {
+          font-size: 6px;
+          color: #fff;
+          background: #3f4954;
+          padding: 0px 12px 2px 12px;
+          margin-right: 20px;
+          border-radius: 2px;
+        }
+
+        span:nth-child(2) {
+          opacity: 0.6;
+          font-size: 5px;
+        }
+      }
+
+      .title-h2 {
+        font-weight: bold;
+        margin-left: 10px;
+        font-size: 6px;
+        margin-bottom: 10px;
+      }
+
+
+    }
+    .price-info {
+      margin-left: 10px;
+        .info-hint {
+          span {
+            border-radius: 2px;
+            color: #fc756d;
+            padding: 0px 2px;
+            background: #f9e9eb;
+            margin-right: 6px;
+          }
+        }
+        .info-price{
+          margin-top: 10px;
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          span:nth-child(1){
+            font-size: 20px;
+            font-weight: bold;
+            color: #fe9b4e;
+            margin-right: 10px;
+          }
+          span:nth-child(2){
+            font-size: 16px;
+            color: #6a5a5a;
+            text-decoration:line-through;
+            margin-left: 10px;
+          }
+          .notice{
+            color: #fff;
+            margin-left: 10px;
+            font-size: 6px;
+            display: flex;
+            padding: 2px 6px;
+            align-items:center;
+            background: red;
+            border-radius: 10px;
+          }
+        }
+      }
+  }
+}
+</style>
--- src/views/favor/favor.vue
+++ src/views/favor/favor.vue
@@ -1,121 +1,37 @@
 <!--
- * @Author: '陈28176' 'chen28176@qq.com'
- * @Date: 2023-02-23 22:10:10
- * @LastEditors: '陈28176' 'chen28176@qq.com'
- * @LastEditTime: 2023-03-06 17:42:15
- * @Description: 
+ * @Description: 订单详情页
+ * @Author: 86
+ * @Date: 2023-02-04 21:47:40
+ * @LastEditTime: 2023-04-19 19:43:32
+ * @LastEditors: Please set LastEditors
+ * @FilePath: \hy-trip\src\views\favor\favor.vue
 -->
 <template>
-  <!-- 收藏 -->
   <div class="favor">
-    <!-- 导航 -->
-    <NavBar :title='title' left-text="首页" @click-left="onClickLeft"/>
-    <van-tabs v-model:active="active">
-      <van-tab title="房屋">
-        <favor1  v-for="index in 6"/>
-      </van-tab>
-      <van-tab title="房东">
-        <div class="favor-landlord">
-          <p class="text">暂无收藏</p>
-        <button class="button" @click="onClickLeft">随便去逛逛</button>
-        </div>
-      </van-tab>
-    </van-tabs>
+    <!-- 导航栏 -->
+    <van-nav-bar title="收藏" />
+    <!-- 下方收藏产品 -->
+    <template v-for="(item,index) in hotFavorList.items" :key="index">
+        <FavorListItem :item-data="item" />
+    </template>
   </div>
-
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { storeToRefs } from 'pinia'
-import NavBar from '@/components/navbar/NavBar.vue';
-import favor1 from './cpns/favor-1.vue';
-import router from '@/router';
-const title = '收藏'
-// 点击返回首页
-const onClickLeft = () => {
-  router.push('/home')
-}
+import FavorListItem from './cpns/favor-list-item.vue'
+import useFavorStore from '@/stores/modules/favor';
+import { storeToRefs } from 'pinia';
 
+// 发送请求
+const favorStore = useFavorStore()
+
+// 获取收藏产品
+favorStore.fetchHotFavorList()
+
+//结构后用于渲染数据到页面
+const { hotFavorList } = storeToRefs(favorStore)
 
 </script>
 
-<style scoped lang="less">
-.landlord-content {
-  .no-data {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-
-    .empty {
-      width: 400px;
-      height: 300px;
-    }
-
-    .btn {
-      width: 160px;
-      height: 40px;
-      border: none;
-      border-radius: 25px;
-      background: #ff9645;
-      color: #fff;
-    }
-  }
-}
-
-.favor{
-  height: 100vh;
-  overflow-x: hidden;
-  margin-bottom: 50px;
-  .favor-landlord{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    .text{
-    margin-top: 200px;
-    font-size: 30px;
-    color: #ff9645;
-    text-align: center;
-  }
-  .button{
-    height: 20px;
-    width:140px;
-    padding: 20px 10px;
-    border-radius: 20px;
-    border: 0px;
-    color: #000;
-    background-color: #ff9645;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  }
-  .favor-houses{
-    margin-top: 30px;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    .top{
-
-      img{
-        width: 340px;
-        height: 200px;
-        border-radius: 20px;
-
-
-      }
-    }
-    .end{
-      display: flex;
-      margin-top: 20px;
-      margin-right:80px;
-      color: #ff9645;
-      .money{
-        color: red;
-      }
-    }
-  }
-}
+<style lang="less" scoped>
 </style>
